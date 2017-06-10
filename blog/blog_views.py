062e7afc17975601daa0438e0b984a0386f3a79f
talithagrace <talithagrace.brown@gmail.com>
2017-06-10 22:13:09 +0100

    added post detail post list and css changes

--- blog/blog_views.py
+++ blog/blog_views.py
@@ -7,6 +7,10 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 class PostViewSet(viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Chain objects """
     queryset = Post.objects.all()
