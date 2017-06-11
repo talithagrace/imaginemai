b70f30c3e721974372ef45cf27e633d9c07499d0
talithagrace <talithagrace.brown@gmail.com>
2017-06-11 23:15:21 +0100

    added post edit and post new forms

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import blog_views
 
 urlpatterns = [
+    url(r'^post/(?P<pk>\d+)/edit/', blog_views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/', blog_views.post_detail, name='post_detail'),
+    url(r'^post/new/', blog_views.post_new, name='post_new'),
+
 ]
